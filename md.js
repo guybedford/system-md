9e953231fda2718f6529c7a78e4f11aff574af67
guybedford <guybedford@gmail.com>
2014-05-14 17:45:32 -0700

    upgrade to new plugin format

--- md.js
+++ md.js
@@ -1,18 +1,10 @@
-var showdown = require('github:coreyti/showdown@0.3.1/src/showdown');
+var showdown = require('showdown@0.3.1');
 var converter = new showdown.converter();
 
-module.exports = function(name, address, fetch, callback, errback) {
-  fetch(address, function(markdown) {
-    callback('module.exports = "'
-    + converter.makeHtml(markdown)
-      .replace(/(["\\])/g, '\\$1')
-      .replace(/[\f]/g, "\\f")
-      .replace(/[\b]/g, "\\b")
-      .replace(/[\n]/g, "\\n")
-      .replace(/[\t]/g, "\\t")
-      .replace(/[\r]/g, "\\r")
-      .replace(/[\u2028]/g, "\\u2028")
-      .replace(/[\u2029]/g, "\\u2029")
-    + '";');
-  }, errback);
+exports.translate = function(load) {
+  return converter.makeHtml(load.source);
+}
+
+exports.instantiate = function(load) {
+  return load.source;
 }
